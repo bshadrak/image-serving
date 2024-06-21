195259d2668db6f582d25a1d6b8fa59f1ab13d58
bshadrak <shadrakbendre3711@gmail.com>
2024-06-21 22:56:59 +0530

    Update provider.tf

--- geo-locations/asia/environments/non-prod/provider.tf
+++ geo-locations/asia/environments/non-prod/provider.tf
@@ -6,16 +6,4 @@ terraform {
     }
   }
 }
-provider "google" {
-  project = "terraform-practice-production"
-  region = "us-west1"
-  zone = "us-west1-a"
-  credentials = "prod.json"
-}
-provider "google" {
-  alias = "us-east1"
-  project = "terraform-practice-production"
-  region = "us-east1"
-  zone = "us-east1-a"
-  credentials = "prod.json"
-}
+provider "google" {}
